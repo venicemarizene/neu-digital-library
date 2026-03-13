daf0f22b9f25f62abd80bbc48448e085d0614164
Linga, Venice Marizene G. <venicemarizene.linga@neu.edu.ph>
2026-03-13 13:32:03 +0000

    without disrupting the codes, UI layout and design, here are the changes

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -32,7 +32,7 @@ function Calendar({
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
-        head_row: "flex",
+        head_row: "flex w-full",
         head_cell:
           "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
