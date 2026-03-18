fae6c75e9150f23673f1848aa96ed864ebf3cbbe
Linga, Venice Marizene G. <venicemarizene.linga@neu.edu.ph>
2026-03-18 18:07:30 +0000

    Without changing the existing codes, UI layout, design, functionalities,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
+          enableSystem={false}
         >
           <FirebaseClientProvider>
             {children}
